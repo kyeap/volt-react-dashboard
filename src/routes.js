57d82a334a0bae0597bbc2fdd9901d8f6b070df1
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2021-04-08 09:12:46 +0100

    make dashboard main page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 
 export const Routes = {
     // pages
-    Presentation: { path: "/" },
-    DashboardOverview: { path: "/dashboard/overview" },
+    Presentation: { path: "/presentation" },
+    DashboardOverview: { path: "/" },
     Transactions: { path: "/transactions" },
     Settings: { path: "/settings" },
     Upgrade: { path: "/upgrade" },
