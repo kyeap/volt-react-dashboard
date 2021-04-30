7c557461fdcb9ff131fde81a17be165d01c77a7f
kyeap <kenjoo.yeap@gmail.com>
2021-04-30 15:47:32 +0100

    fix react query update at date change

--- src/data/grants.js
+++ src/data/grants.js
@@ -2,13 +2,16 @@ import React from "react";
 import { useQuery } from "react-query";
 
 export const useFetchAll = (startDateStr, endDateStr, params) => {
-  console.log(startDateStr, endDateStr);
-  const { isLoading, error, data } = useQuery("grantDataAll", () =>
-    fetch(
-      ` http://grantreportflaskdev/?from_date=${startDateStr}&to_date=${endDateStr}`
-    )
-      .then((res) => res.json())
-      .then((res) => res.data)
+  console.log("in the useFetchAll function", startDateStr, endDateStr);
+  const { isLoading, error, data } = useQuery(
+    ["grantDataAll", startDateStr, endDateStr],
+    () =>
+      fetch(
+        ` http://grantreportflaskdev/?from_date=${startDateStr}&to_date=${endDateStr}`
+        // `http://localhost:5000/?from_date=${startDateStr}&to_date=${endDateStr}`
+      )
+        .then((res) => res.json())
+        .then((res) => res.data)
   );
   console.log("data", data);
   if (params != undefined && data != undefined) {
